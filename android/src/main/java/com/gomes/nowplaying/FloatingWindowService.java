5464c83ca77e57f7e5a125bd012dd62d9e3e791b
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2021-09-04 01:48:28 +0900

    Update view and Add foreground

--- android/src/main/java/com/gomes/nowplaying/FloatingWindowService.java
+++ android/src/main/java/com/gomes/nowplaying/FloatingWindowService.java
@@ -1,16 +1,23 @@
 package com.gomes.nowplaying;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.PixelFormat;
+import android.os.Build;
 import android.os.IBinder;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
+
+import androidx.core.app.NotificationCompat;
 
 import com.gomes.NowPlaying.R;
 
@@ -21,14 +28,13 @@ import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodChannel;
 
 public class FloatingWindowService extends Service implements View.OnClickListener {
+    public static final String CHANNEL_ID = "ForegroundServiceChannel";
+    private static final int NOTIFICATION_ID = 1;
     static public final String SHARED_PREFS_KEY = "com.example.p_lyric.floating.window.service";
     static public final String IS_APP_VISIBLE_KEY = "isAppVisible";
     static private final String LYRICS_KEY = "lyrics";
     static private final String TITLE_KEY = "title";
     static private final String ARTIST_KEY = "artist";
-
-    // TODO: 백그라운드에서 곡 변경시 가사 업데이트하기
-    static boolean isDisplayed = false;
 
     private WindowManager mWindowManager;
     private View mFloatingView;
@@ -42,12 +48,110 @@ public class FloatingWindowService extends Service implements View.OnClickListen
 
     @Override
     public void onCreate() {
-        super.onCreate();
+        createNotificationChannel();
+        Intent notificationIntent = getAppStartingIntent();
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0, notificationIntent, PendingIntent.FLAG_IMMUTABLE);
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("PLyric 가사 창 실행 중")
+                .setContentText("탭하여 앱 실행")
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentIntent(pendingIntent)
+                .build();
+        startForeground(NOTIFICATION_ID, notification);
+    }
 
-        //getting the widget layout from xml using layout inflater
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Foreground Service Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            assert manager != null;
+            manager.createNotificationChannel(serviceChannel);
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (mFloatingView == null) {
+            createWindowView();
+
+        } else {
+            setLyricsTextView();
+        }
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (mFloatingView != null) {
+            mWindowManager.removeView(mFloatingView);
+            mFloatingView = null;
+        }
+
+        NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        assert notificationManager != null;
+        notificationManager.cancel(NOTIFICATION_ID);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int viewId = v.getId();
+
+        if (viewId == R.id.collapsed_close_button || viewId == R.id.expanded_close_button) {
+            stopSelf();
+        } else if (viewId == R.id.expanded_fullscreen_button) {
+            startApp();
+        } else if (viewId == R.id.expanded_collapse_button) {//switching views
+            collapsedView.setVisibility(View.VISIBLE);
+            expandedView.setVisibility(View.GONE);
+        } else if (viewId == R.id.collapsed_expand_button) {
+            collapsedView.setVisibility(View.GONE);
+            expandedView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    /**
+     * 앱을 화면의 최상단에 띄운다.
+     */
+    private void startApp() {
+        this.startActivity(getAppStartingIntent());
+    }
+
+    private Intent getAppStartingIntent() {
+        Intent startIntent = this
+                .getPackageManager()
+                .getLaunchIntentForPackage(this.getPackageName());
+
+        if (startIntent != null)
+            startIntent.setFlags(
+                    Intent.FLAG_ACTIVITY_REORDER_TO_FRONT |
+                            Intent.FLAG_ACTIVITY_NEW_TASK |
+                            Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED
+            );
+        return startIntent;
+    }
+
+    private void setLyricsTextView() {
+        if (mFloatingView == null) return;
+
+        SharedPreferences prefs = this.getSharedPreferences(FloatingWindowService.SHARED_PREFS_KEY, Context.MODE_PRIVATE);
+        String lyrics = prefs.getString(FloatingWindowService.LYRICS_KEY, "");
+        TextView textView = mFloatingView.findViewById(R.id.expanded_textView);
+
+        if (!lyrics.isEmpty()) textView.setText(lyrics);
+    }
+
+    private void createWindowView() {
+        // getting the widget layout from xml using layout inflater
         mFloatingView = LayoutInflater.from(this).inflate(R.layout.layout_floating_window, null);
 
-        //setting the layout parameters
+        // setting the layout parameters
         final WindowManager.LayoutParams params;
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             params = new WindowManager.LayoutParams(
@@ -65,27 +169,30 @@ public class FloatingWindowService extends Service implements View.OnClickListen
                     PixelFormat.TRANSLUCENT);
         }
 
-        //getting windows services and adding the floating view to it
+        // getting windows services and adding the floating view to it
         mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
         mWindowManager.addView(mFloatingView, params);
 
-        //getting the collapsed and expanded view from the floating view
+        setLyricsTextView();
+
+        // getting the collapsed and expanded view from the floating view
         collapsedView = mFloatingView.findViewById(R.id.layoutCollapsed);
         expandedView = mFloatingView.findViewById(R.id.layoutExpanded);
 
-        //adding click listener to close button and expanded view
-        mFloatingView.findViewById(R.id.buttonClose).setOnClickListener(this);
-        expandedView.setOnClickListener(this);
+        // 클릭 리스너 추가
+        mFloatingView.findViewById(R.id.collapsed_close_button).setOnClickListener(this);
+        mFloatingView.findViewById(R.id.collapsed_expand_button).setOnClickListener(this);
+        mFloatingView.findViewById(R.id.expanded_close_button).setOnClickListener(this);
+        mFloatingView.findViewById(R.id.expanded_fullscreen_button).setOnClickListener(this);
+        mFloatingView.findViewById(R.id.expanded_collapse_button).setOnClickListener(this);
 
-        SharedPreferences prefs = this.getSharedPreferences(FloatingWindowService.SHARED_PREFS_KEY, Context.MODE_PRIVATE);
-        String lyrics = prefs.getString(FloatingWindowService.LYRICS_KEY, "");
-        TextView textView = mFloatingView.findViewById(R.id.expanded_textView);
-        if (!lyrics.isEmpty()) textView.setText(lyrics);
+        // adding an touch listener to make drag movement of the floating widget
+        collapsedView.setOnTouchListener(getWindowTouchListener(params));
+        expandedView.setOnTouchListener(getWindowTouchListener(params));
+    }
 
-        isDisplayed = true;
-
-        //adding an touchlistener to make drag movement of the floating widget
-        mFloatingView.findViewById(R.id.relativeLayoutParent).setOnTouchListener(new View.OnTouchListener() {
+    private View.OnTouchListener getWindowTouchListener(WindowManager.LayoutParams params) {
+        return new View.OnTouchListener() {
             private int initialX;
             private int initialY;
             private float initialTouchX;
@@ -101,13 +208,6 @@ public class FloatingWindowService extends Service implements View.OnClickListen
                         initialTouchX = event.getRawX();
                         initialTouchY = event.getRawY();
                         return true;
-
-                    case MotionEvent.ACTION_UP:
-                        //when the drag is ended switching the state of the widget
-                        collapsedView.setVisibility(View.GONE);
-                        expandedView.setVisibility(View.VISIBLE);
-                        return true;
-
                     case MotionEvent.ACTION_MOVE:
                         //this code is helping the widget to move around the screen with fingers
                         params.x = initialX + (int) (event.getRawX() - initialTouchX);
@@ -117,28 +217,7 @@ public class FloatingWindowService extends Service implements View.OnClickListen
                 }
                 return false;
             }
-        });
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        isDisplayed = false;
-        if (mFloatingView != null) {
-            mWindowManager.removeView(mFloatingView);
-            mFloatingView = null;
-        }
-    }
-
-    @Override
-    public void onClick(View v) {
-        int id = v.getId();
-        if (id == R.id.layoutExpanded) {//switching views
-            collapsedView.setVisibility(View.VISIBLE);
-            expandedView.setVisibility(View.GONE);
-        } else if (id == R.id.buttonClose) {//closing the widget
-            stopSelf();
-        }
+        };
     }
 
     // TODO(민성): SharedPreference 에서 플로팅을 사용하기로 한 경우만 띄우도록 하기
